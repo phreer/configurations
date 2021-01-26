7094b1a4a8d24144a31e32a5af1f82c7a2b3cbd7
Phree Liu <iphreeliu@gmail.com>
2021-01-26 13:09:01 +0800

    Add setup code for XWindow.

--- unix-scripts/wsl2-init.sh
+++ unix-scripts/wsl2-init.sh
@@ -39,3 +39,7 @@ function _fish_collapsed_pwd() {
     echo "${elements[*]}"
 }
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(_fish_collapsed_pwd)\[\033[00m\]\$ '
+
+# Setup XWindow server.
+export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+export LIBGL_ALWAYS_INDIRECT=1
