b78248a37841286511bb879d2e507210509b58e9
Phree Liu <iphreeliu@gmail.com>
2022-12-28 22:41:26 +0800

    Add USTC's mirror of HomeBrew.

--- unix-scripts/osx-init.sh
+++ unix-scripts/osx-init.sh
@@ -8,3 +8,9 @@ export PATH=$HOME/local/bin:$PATH
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+# Use USTC's mirror for HomeBrew
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+
