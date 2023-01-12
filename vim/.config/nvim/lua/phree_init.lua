f99fb163e04b3cd5dd9b89df07a9122eb6b06a9c
Weifeng Liu <liu.wf@gmail.com>
2023-01-12 15:16:44 +0800

    neovim: add <leader>\r keymap to resume telescope result

--- vim/.config/nvim/lua/phree_init.lua
+++ vim/.config/nvim/lua/phree_init.lua
@@ -1,9 +1,12 @@
 local vim_map = vim.api.nvim_set_keymap
 
-
 -- empty setup using defaults: add your own options
 require('nvim-tree').setup {
 }
+
+-- ns for noremap and silent
+local map_options_ns = {noremap = true, silent = true}
+vim_map('n', '<C-n>', ':NvimTreeToggle<CR>', map_options_ns)
 
 require('lualine').setup {
   options = {
@@ -11,23 +14,11 @@ require('lualine').setup {
   }
 }
 
-require('telescope').setup {
-  pickers = {
-    find_files = {
-      follow = true,
-    }
-  },
-}
-
 local opt = require("toggleterm").setup({
     open_mapping = [[<Leader>t]],
     start_in_insert = true,
     direction = 'horizontal',
 })
-
--- ns for noremap and silent
-local map_options_ns = {noremap = true, silent = true}
-vim_map('n', '<C-n>', ':NvimTreeToggle<CR>', map_options_ns)
 
 require('barbar_setting')
 require('telescope_setting')
