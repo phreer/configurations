55aaaa1e48fba4647562061725b087e7eaaa0573
Weifeng Liu <liu.wf@outlook.com>
2023-02-09 19:33:55 +0800

    setup_tap.sh: use iptables-legacy to create rules

--- unix-scripts/virtualization/setup_tap.sh
+++ unix-scripts/virtualization/setup_tap.sh
@@ -5,6 +5,6 @@ sudo ip link set crosvm_tap up
 sudo sysctl net.ipv4.ip_forward=1
 # Network interface used to connect to the internet.
 HOST_DEV=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
-sudo iptables -t nat -A POSTROUTING -o "${HOST_DEV}" -j MASQUERADE
-sudo iptables -A FORWARD -i "${HOST_DEV}" -o crosvm_tap -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i crosvm_tap -o "${HOST_DEV}" -j ACCEPT
+sudo iptables-legacy -t nat -A POSTROUTING -o "${HOST_DEV}" -j MASQUERADE
+sudo iptables-legacy -A FORWARD -i "${HOST_DEV}" -o crosvm_tap -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables-legacy -A FORWARD -i crosvm_tap -o "${HOST_DEV}" -j ACCEPT
