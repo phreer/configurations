d0ff800a709a60b4b76156c8978d4d2a1e17fc48
Weifeng Liu <liu.wf@outlook.com>
2023-03-14 22:41:22 +0800

    init: fix bug in init script for macOS.

--- unix-scripts/osx-init.sh
+++ unix-scripts/osx-init.sh
@@ -2,7 +2,7 @@ alias ll="ls -alf"
 alias grep="grep --color"
 export CLICOLOR=1
 
-export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/local/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$HOME/local/lib
 export LIBRARY_PATH=$LIBRARY_PATH:$HOME/local/lib
 export PATH=$HOME/local/bin:$PATH
 
