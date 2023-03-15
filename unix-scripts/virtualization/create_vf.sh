47317ac591f4270fe77c7ad6074d3779365c7921
Weifeng Liu <liu.wf@outlook.com>
2023-03-15 12:10:33 +0800

    virt: add scripts to create vf from i915

---
+++ unix-scripts/virtualization/create_vf.sh
@@ -0,0 +1,13 @@
+cd /sys/class/drm/card0/
+echo 0     | sudo tee iov/pf/device/sriov_drivers_auutoprobe
+echo 1     | sudo tee iov/pf/device/sriov_numvfs
+echo 4     | sudo tee iov/pf/gt/exec_quantum_ms
+echo 4000  | sudo tee iov/pf/gt/preempt_timeout_us
+echo 20    | sudo tee iov/vf1/gt/exec_quantum_ms
+echo 20000 | sudo tee iov/vf1/gt/preempt_timeout_us
+echo 1     | sudo tee iov/pf/device/sriov_drivers_auutoprobe
+
+cd -
+./set_vf_param.sh
+sudo modprobe vfio-pci
+sudo ./vfio-pci-bind/vfio-pci-bind.sh 0000:00:02.1
