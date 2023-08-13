4a47bf080d39af4643bf514a845fd1b45c2ce555
Weifeng Liu <weifeng.liu.z@outlook.com>
2023-08-13 23:38:38 +0800

    scripts: Move iov to prelim_iov

--- unix-scripts/virtualization/create_vf.sh
+++ unix-scripts/virtualization/create_vf.sh
@@ -1,11 +1,11 @@
 cd /sys/class/drm/card0/
-echo 0     | sudo tee iov/pf/device/sriov_drivers_auutoprobe
-echo 1     | sudo tee iov/pf/device/sriov_numvfs
-echo 4     | sudo tee iov/pf/gt/exec_quantum_ms
-echo 4000  | sudo tee iov/pf/gt/preempt_timeout_us
-echo 20    | sudo tee iov/vf1/gt/exec_quantum_ms
-echo 20000 | sudo tee iov/vf1/gt/preempt_timeout_us
-echo 1     | sudo tee iov/pf/device/sriov_drivers_auutoprobe
+echo 0     | sudo tee prelim_iov/pf/device/sriov_drivers_auutoprobe
+echo 1     | sudo tee prelim_iov/pf/device/sriov_numvfs
+echo 4     | sudo tee prelim_iov/pf/gt/exec_quantum_ms
+echo 4000  | sudo tee prelim_iov/pf/gt/preempt_timeout_us
+echo 20    | sudo tee prelim_iov/vf1/gt/exec_quantum_ms
+echo 20000 | sudo tee prelim_iov/vf1/gt/preempt_timeout_us
+echo 1     | sudo tee prelim_iov/pf/device/sriov_drivers_auutoprobe
 
 cd -
 ./set_vf_param.sh
--- unix-scripts/virtualization/set_vf_param.sh
+++ unix-scripts/virtualization/set_vf_param.sh
@@ -1,5 +1,5 @@
-cd /sys/class/drm/card0/
-echo 40     | sudo tee iov/pf/gt/exec_quantum_ms
-echo 40000  | sudo tee iov/pf/gt/preempt_timeout_us
-echo 200    | sudo tee iov/vf1/gt/exec_quantum_ms
-echo 200000 | sudo tee iov/vf1/gt/preempt_timeout_us
+CARD_PATH='/sys/bus/pci/devices/0000:00:02.0/drm/card0/'
+echo 40     | sudo tee $CARD_PATH/prelim_iov/pf/gt0/exec_quantum_ms
+echo 40000  | sudo tee $CARD_PATH/prelim_iov/pf/gt0/preempt_timeout_us
+echo 200    | sudo tee $CARD_PATH/prelim_iov/vf1/gt0/exec_quantum_ms
+echo 200000 | sudo tee $CARD_PATH/prelim_iov/vf1/gt0/preempt_timeout_us
