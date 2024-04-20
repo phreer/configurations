3b87355810cddf667ff9fce6aaae873d5fa17d83
Weifeng Liu <iphreeliu@gmail.com>
2024-04-20 16:54:12 +0800

    script: Copy config files instead of linking them
    Configuration files like ssh config and git config are frequently
    updated per machine, then just make a standalone version for them!
    
    Signed-off-by: Weifeng Liu <iphreeliu@gmail.com>

--- unix-scripts/create_links.sh
+++ unix-scripts/create_links.sh
@@ -17,6 +17,17 @@ fi
 INIT_FILENAME="$PREFIX""-init.sh"
 SSH_CONFIG_FILE="$PREFIX"-config
 
+function CopyFileIfNotExist() {
+  local src=$1
+  local dst=$2
+  if [ ! -e "$2" ]; then
+    cp "$src" "$dst"
+    echo "Copy [$src] to [$dst]"
+  else
+    echo "File [$dst] exists, skip copying"
+  fi
+}
+
 function CreateSymbolicLink() {
   local force=$1
   local target=$2
@@ -32,6 +43,7 @@ mkdir -p "$HOME"/.config/clash 2>/dev/null
 mkdir -p "$HOME"/.config/fontconfig/conf.d 2>/dev/null
 mkdir -p "$HOME"/.ssh 2>/dev/null
 mkdir -p "$HOME"/local/bin 2>/dev/null
+mkdir -p "$HOME"/.local/share/fonts 2>/dev/null
 
 CreateSymbolicLink 1 "$SCRIPT_DIR/$INIT_FILENAME" "$HOME/init.sh"
 source_command1='. $HOME/init.sh'
@@ -42,24 +54,26 @@ if ! grep -E "$source_command1"\|"$source_command2" "$HOME"/.bashrc >/dev/null; 
 fi
 
 # Link directory
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/nvim "$HOME"/.config/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/emacs "$HOME"/.config/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/tmux "$HOME"/.config/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/alacritty "$HOME"/.config/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../fonts/NerdFontSymbols "$HOME"/.local/share/fonts/
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/nvim "$HOME"/.config/
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/emacs "$HOME"/.config/
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/tmux "$HOME"/.config/
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/alacritty "$HOME"/.config/
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../fonts/NerdFontSymbols "$HOME"/.local/share/fonts/
+
 # Only link some files
 CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/clash/ruleset "$HOME"/.config/clash/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.config/fontconfig/conf.d/10-custom.conf \
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/fontconfig/conf.d/10-custom.conf \
     "$HOME"/.config/fontconfig/conf.d/
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../vim/.vimrc "$HOME"/.vimrc
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../vim/.vimrc "$HOME"/.vimrc
 CreateSymbolicLink 1 "$SCRIPT_DIR"/../vim/.vim "$HOME"/.vim
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../.ssh/$SSH_CONFIG_FILE "$HOME"/.ssh/config
-CreateSymbolicLink 1 "$SCRIPT_DIR"/../git/.gitconfig "$HOME"
 CreateSymbolicLink 1 "$SCRIPT_DIR"/proxy.sh "$HOME"
 CreateSymbolicLink 1 "$SCRIPT_DIR"/smart-pinentry.sh "$HOME"/local/bin/pinentry
+CreateSymbolicLink 0 "$SCRIPT_DIR"/../.config/systemd/user/uxplay.service \
+    "$HOME"/.config/systemd/user/uxplay.service
 
-cp "$SCRIPT_DIR"/../.config/clash/config.yaml "$HOME"/.config/clash/config.yaml
+CopyFileIfNotExist "$SCRIPT_DIR"/../.ssh/$SSH_CONFIG_FILE "$HOME"/.ssh/config
+CopyFileIfNotExist "$SCRIPT_DIR"/../git/.gitconfig "$HOME"/.gitconfig
+CopyFileIfNotExist "$SCRIPT_DIR"/../.config/clash/config.yaml "$HOME"/.config/clash/config.yaml
 
-cp "$SCRIPT_DIR"/../.config/systemd/user/uxplay.service "$HOME"/.config/systemd/user/uxplay.service
 echo "Run systemctl enable --user uxplay.service to enable uxplay"
 echo "Run systemctl start --user uxplay.service to start uxplay"
