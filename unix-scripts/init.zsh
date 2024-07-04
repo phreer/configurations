4fee3a766d2bcb24a8aaa9a10d09dc2239aa4b72
Weifeng Liu <weifeng.liu.z@gmail.com>
2024-07-04 16:16:37 +0800

    scripts: Make key binding of M-Backspace consistent with bash
    Signed-off-by: Weifeng Liu <weifeng.liu.z@gmail.com>

--- unix-scripts/init.zsh
+++ unix-scripts/init.zsh
@@ -1,5 +1,10 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
+# Make key binding of M-Backspace consistent with bash, namely delete backward
+# until white space or splash.
+autoload -U select-word-style
+select-word-style bash
+
 # Initialize command prompt
 export PS1="%F{blue}%n@%m%f:%F{green}%~%f%# "
 
