b639a8a4e391b1321681246ce8cf56c1174dfa06
Weifeng Liu <weifeng.liu.z@gmail.com>
2024-07-12 10:03:40 +0800

    scripts/init: Use antigen to manage plugins for zsh
    ...also enable three useful plugins: zsh-completions, zsh-syntax-highlighting
    and command-not-found.
    
    Signed-off-by: Weifeng Liu <weifeng.liu.z@gmail.com>

--- unix-scripts/init.zsh
+++ unix-scripts/init.zsh
@@ -1,5 +1,13 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
+load_antigen_bundles() {
+  antigen bundle zsh-users/zsh-completions
+  antigen bundle zsh-users/zsh-syntax-highlighting
+  antigen bundle command-not-found
+
+  antigen apply
+}
+
 # Make key binding of M-Backspace consistent with bash, namely delete backward
 # until white space or splash.
 autoload -U select-word-style
@@ -9,3 +17,9 @@ select-word-style bash
 export PS1="%F{blue}%n@%m%f:%F{green}%~%f%# "
 
 bindkey -s '\eo' 'cd ..\n'
+
+# antigen for plugin management
+if [ -e $HOME/antigen.zsh ]; then
+  source $HOME/antigen.zsh
+  load_antigen_bundles
+fi
