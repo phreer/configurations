8c977dbd70d42a727ccc1c08e70f7ed7652d583b
Weifeng Liu <weifeng.liu@intel.com>
2025-10-13 15:37:51 +0800

    nvim: Change keybinding of auto completion to C-Tab

--- .config/nvim/lua/config_nvim_cmp.lua
+++ .config/nvim/lua/config_nvim_cmp.lua
@@ -62,10 +62,9 @@ return function()
           fallback()
         end
       end, {'i', 's'}),
-  
-      ['<Tab>'] = cmp.mapping(function(fallback)
+
+      ['<C-Tab>'] = cmp.mapping(function(fallback)
         local col = vim.fn.col('.') - 1
-  
         if cmp.visible() then
           cmp.select_next_item(select_opts)
         elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
@@ -74,7 +73,7 @@ return function()
           cmp.complete()
         end
       end, {'i', 's'}),
-  
+
       ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item(select_opts)
