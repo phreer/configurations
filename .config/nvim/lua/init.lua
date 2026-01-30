15ea15ff9aba940794f17b589857d443f5dd0d37
Weifeng Liu <weifeng.liu.z@gmail.com>
2026-01-30 14:44:54 +0800

    config/nvim: Enable OSC 52 sequence for clipboard handling

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,2 +1,19 @@
 require('lazy_init')
 require('text-filter').setup()
+
+-- 将系统剪切板与 Neovim 剪切板打通
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has('nvim-0.10') == 1 then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
