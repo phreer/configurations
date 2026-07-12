ba9af11e8cc41b8552a95418b90c98204b0554a4
Weifeng Liu <weifeng.liu.z@gmail.com>
2026-07-12 21:50:50 +0800

    scripts: Add install_gnome_extensions.sh

---
+++ unix-scripts/install_gnome_extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! command -v gext &>/dev/null; then
+  echo "Command gext doesn't exist, please install with pip"
+fi
+
+gext install BingWallpaper@ineffable-gmail.com
+gext install Vitals@CoreCoding.com
+gext install marcs14@gmail.com
+gext install clipboard-indicator@tudmotu.com
+gext install trayIconsReloaded@selfmade.pl
